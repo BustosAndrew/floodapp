f987d1ef3517e81e1a5afc3340cfe77833e67568
rickNrip <117496632+rickNrip@users.noreply.github.com>
2024-05-04 12:43:56 -0700

    fb add

--- lib/UI/add.dart
+++ lib/UI/add.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flood/model/supply.dart';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AddPage extends StatelessWidget {
-  void submitData(BuildContext context) {
+  // Creating text editing controllers to capture input
+  final TextEditingController _waterController = TextEditingController();
+  final TextEditingController _foodController = TextEditingController();
+  final TextEditingController _energyController = TextEditingController();
+
+  void submitData(BuildContext context) async {
+    // Capture the input data
+    double waterLevel = double.tryParse(_waterController.text) ?? 0.0;
+    double foodCount = double.tryParse(_foodController.text) ?? 0.0;
+    double energyHours = double.tryParse(_energyController.text) ?? 0.0;
+
     // Assuming you have a method to submit data
     SupplyData newData = SupplyData(
-      waterLevel: 0.8,
-      foodLevel: 0.5,
-      energyLevel: 0.3,
+      waterLevel: waterLevel,
+      foodLevel: foodCount,
+      energyLevel: energyHours,
     );
 
-    // Navigate back to the HomeScreen and pass the data
-    Navigator.pop(context, newData);
+    // Add the data to Firestore
+    DocumentReference ref =
+        FirebaseFirestore.instance.collection('supplies').doc();
+    await ref.set({
+      'water': waterLevel,
+      'food': foodCount,
+      'energy': energyHours,
+      'timestamp':
+          FieldValue.serverTimestamp(), // To add a timestamp when data is added
+    });
+
+    // Navigate back to the HomeScreen or show a success message
+    Navigator.pop(context);
   }
 
   @override
@@ -20,7 +44,7 @@ class AddPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text(
-            '',
+            'Add Supplies',
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.w500,
@@ -52,8 +76,7 @@ class AddPage extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.only(top: 49, left: 9, bottom: 200),
                   child: Row(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center, // Center the row horizontally
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
                         width: 363,
@@ -64,93 +87,12 @@ class AddPage extends StatelessWidget {
                         ),
                         child: Column(
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 30, bottom: 20, top: 20),
-                                  child: Text(
-                                    "Water",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 20),
-                                Container(
-                                  width: 150,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        bottom: 20, top: 20),
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        hintText: 'Enter gallons',
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 30, bottom: 20, top: 20),
-                                  child: Text(
-                                    "Food",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 20),
-                                Container(
-                                  width: 150,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        bottom: 20, top: 20),
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        hintText: 'Enter # of cans',
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 30, bottom: 20, top: 20),
-                                  child: Text(
-                                    "Energy",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 20),
-                                Container(
-                                  width: 150,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        bottom: 20, top: 20),
-                                    child: TextField(
-                                      decoration: InputDecoration(
-                                        hintText: 'Enter hours',
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                            _buildInputField(
+                                'Water', 'Enter gallons', _waterController),
+                            _buildInputField(
+                                'Food', 'Enter # of cans', _foodController),
+                            _buildInputField(
+                                'Energy', 'Enter hours', _energyController),
                             SizedBox(height: 15),
                             ElevatedButton(
                               onPressed: () => submitData(context),
@@ -165,8 +107,7 @@ class AddPage extends StatelessWidget {
                               style: ButtonStyle(
                                 backgroundColor:
                                     MaterialStateProperty.all<Color>(
-                                        Color.fromRGBO(97, 173, 235,
-                                            1)), // Set the background color
+                                        Color.fromRGBO(97, 173, 235, 1)),
                               ),
                             )
                           ],
@@ -180,6 +121,38 @@ class AddPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildInputField(
+      String label, String hintText, TextEditingController controller) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 30, bottom: 20, top: 20),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        SizedBox(width: 20),
+        Container(
+          width: 150,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20, top: 20),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: hintText,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
