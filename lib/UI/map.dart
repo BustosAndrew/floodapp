738a6b45fe133dc68471615ef45e234e09129582
rickNrip <117496632+rickNrip@users.noreply.github.com>
2024-05-04 11:41:29 -0700

    wooorj

--- lib/UI/map.dart
+++ lib/UI/map.dart
@@ -8,6 +8,8 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   int _selectedIndex = 0;
+  String _currentImage =
+      'images/MapSafeZones.png'; // Ensure this path is correct
 
   void _onItemTapped(int index) {
     setState(() {
@@ -15,42 +17,74 @@ class _MapPageState extends State<MapPage> {
     });
   }
 
+  void _handleMenuSelection(String choice) {
+    setState(() {
+      _currentImage =
+          'images/MapSafeZones.png'; // Modify to change the map image based on selection
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('New Page'),
-        ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [
-                Color(0xFF263DA8), // Light blue at the bottom
-                Color(0xFF0E1A68), // Darker blue at the top
-              ],
-            ),
-          ),
-          child: Center(
-            child: Container(
-              width: 350,
-              height: 550,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                border: Border.all(color: Colors.black, width: 3),
-              ),
-              child: Center(
-                child: Text('Place your image here',
-                    style: TextStyle(fontSize: 16)),
-              ),
-            ),
+      appBar: AppBar(
+        title: Text('Interactive Map Viewer'),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [Color(0xFF263DA8), Color(0xFF0E1A68)],
           ),
         ),
-        bottomNavigationBar: CustomBottomNavBar(
-          selectedIndex: _selectedIndex,
-          onItemTapped: _onItemTapped,
-        ));
+        child: Stack(
+          children: [
+            Center(
+              child: InteractiveViewer(
+                boundaryMargin: EdgeInsets.all(20),
+                minScale: 0.5,
+                maxScale: 4.0,
+                child: Container(
+                  width: 700, // Adjust based on your needs
+                  height: 1100, // Adjust based on your needs
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    border: Border.all(color: Colors.black, width: 3),
+                  ),
+                  child: Image.asset(
+                    _currentImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 20,
+              right: 20,
+              child: PopupMenuButton<String>(
+                onSelected: _handleMenuSelection,
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                  PopupMenuItem<String>(
+                    value: 'Option 1',
+                    child: Text('Option 1'),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Option 2',
+                    child: Text('Option 2'),
+                  ),
+                ],
+                icon: Icon(Icons.menu, size: 30, color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
   }
 }
