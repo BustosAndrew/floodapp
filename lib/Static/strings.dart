4b7ca9ac5acea150fb82f209ea40dac9ed9acc3a
CodingEmpire <codykneale@yahoo.com>
2024-05-04 09:21:22 -0700

    Map

--- lib/Static/strings.dart
+++ lib/Static/strings.dart
@@ -1,1 +1,20 @@
-
+var seasonCurrent = "Current Season";
+var supplyWater = "Water";
+var supplyFood = "Food";
+var supplyElectricity = "Supply Electricity";
+var friendSupply = "Friend Supply Levels";
+var disasterLevel = "Disaster Level";
+var disasterLow = "Low";
+var disasterMedium = "Medium";
+var disasterHigh = "High";
+var supplyLevels = "Supply Levels";
+var supplyLevelsWater = "Water";
+var supplyLevelsWaterQues = "(gallons)";
+var supplyLevelsFood = "Food";
+var supplyLevelsFoodQues = "(cans)";
+var supplyLevelsElectricity = "Electricity";
+var supplyLevelsElectricityQues = "(Hours)";
+var chatQuestion = "Ask your question here";
+var friend1 = "Friend 1";
+var friend2 = "Friend 2";
+var friend3 = "Friend 3";
